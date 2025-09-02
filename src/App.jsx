adcd556dc6088d906f0e0edd31701901a5137b9f
Ayomikun Oyeniyi <ayomikunoyeniyi07@gmail.com>
2025-09-02 16:30:15 -0500

    Setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Experience from './components/Experience'
+import Work from './components/Work'
+import General from './components/General'
+import './App.css'
+
+function App() {
+  
+  return (
+    <div>
+     <General></General>
+     <Work></Work>
+     <Experience></Experience>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/General.jsx
@@ -0,0 +1,25 @@
+export default function General() {
+    return (
+        <div className="general">
+            <div>
+                <label htmlFor="Name">Name:</label>
+                <input type="text" name="Name" id="full-name"/>
+            </div>
+            
+            <div>
+                <label htmlFor="Email">Email Address:</label>
+                <input type="email" name="Email" id="email" />
+            </div>
+            
+            <div>
+                <label htmlFor="Telephone"></label>
+                <input type="tel" name="Telephone" id="phone-number" />
+            </div>
+
+            <div>
+                <button type="button">Submit</button>
+                <button type="button">Edit</button>
+            </div>
+        </div>
+    );
+}
