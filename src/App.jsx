dc786e8f3899b368faac247608ca5c3abd248591
Ayomikun Oyeniyi <ayomikunoyeniyi07@gmail.com>
2025-09-03 23:16:00 -0500

    Restructure General and App components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import UserInput from './components/UserInput';
+import CV from './components/CV';
 import './App.css';
 
 function App() {
   
   return (
     <>
-     <UserInput></UserInput>
-     <CV></CV>
+    <div className='user-input'>
+      <UserInput></UserInput>
+    </div>
+    <div className='cv-container'>
+      <CV></CV>
+    </div>
     </>
   )
 }
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,11 +1,9 @@
-import Experience from "./Experience";
 import General from "./General";
-import Work from "./Work";
 
 export default function UserInput() {
     return (
-        <div>
-            <General></General>
-        </div>
+        <>
+            <General>General</General>
+        </>
     );
 }
