b524478b7604b7b55e04f6cbe5b0865c4c66f8ea
Ayomikun Oyeniyi <ayomikunoyeniyi07@gmail.com>
2025-09-03 22:34:32 -0500

    Create components for restructured layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import Experience from './components/Experience'
-import Work from './components/Work'
-import General from './components/General'
-import './App.css'
+import UserInput from './components/UserInput';
+import './App.css';
 
 function App() {
   
   return (
-    <div>
-     <General></General>
-     <Work></Work>
-     <Experience></Experience>
-    </div>
+    <>
+     <UserInput></UserInput>
+     <CV></CV>
+    </>
   )
 }
 
---
+++ src/components/UserInput.jsx
@@ -0,0 +1,11 @@
+import Experience from "./Experience";
+import General from "./General";
+import Work from "./Work";
+
+export default function UserInput() {
+    return (
+        <div>
+            <General></General>
+        </div>
+    );
+}
