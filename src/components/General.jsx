65a9dae04d0dd6e8f635dcfe6d1ab045fa549f3b
Ayomikun Oyeniyi <ayomikunoyeniyi07@gmail.com>
2025-09-04 09:54:41 -0500

    Add basic styling for General section

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -3,24 +3,26 @@ import '../styles/General.css';
 export default function General({children}) {
     return (
         <div className="general">
-            <h1>{children}</h1>
-            <div>
-                <label htmlFor="full-name">Name</label>
-                <input type="text" name="name" id="full-name"/>
+            <h2>{children}</h2>
+            <div className='gen-info'>
+                <div className='name-holder'>
+                    <label htmlFor="full-name">Name</label>
+                    <input type="text" name="name" id="full-name"/>
+                </div>
+                
+                <div className='email-holder'>
+                    <label htmlFor="email">Email Address</label>
+                    <input type="email" name="email" id="email" />
+                </div>
             </div>
             
-            <div>
-                <label htmlFor="email">Email Address</label>
-                <input type="email" name="email" id="email" />
-            </div>
-            
-            <div>
+            <div className='tel-holder'>
                 <label htmlFor="phone-number">Telephone</label>
                 <input type="tel" name="telephone" id="phone-number" />
             </div>
 
-            <div>
-                <button type="button">Submit</button>
+            <div className=''>
+                <button type="button">Save</button>
                 <button type="button">Edit</button>
             </div>
         </div>
