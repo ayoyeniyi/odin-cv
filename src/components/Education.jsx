b6d9b01ddf1a591f33080c98ec68c19efd446e35
Ayomikun Oyeniyi <ayomikunoyeniyi07@gmail.com>
2025-09-18 18:24:32 -0500

    Edit and style Education component

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
+import '../styles/Education.css';
 
-export default function Experience({ children }) {
+export default function Education({ children }) {
     const [saved, setSaved] = useState(false);
 
     function handleSave() {
@@ -10,22 +11,24 @@ export default function Experience({ children }) {
     return (
         <div className="education">
             <h2>{children}</h2>
-            <div>
-                <label htmlFor="sch-name">School</label>
-                <input type="text" name="sch-name" id="sch-name"/>
+            <div className="sch-info">
+                <div className='sch-holder'>
+                    <label htmlFor="sch-name">School</label>
+                    <input type="text" name="sch-name" id="sch-name"/>
+                </div>
+            
+                <div className="mj-holder">
+                    <label htmlFor="major">Major</label>
+                    <input type="text" name="sch-major" id="sch-major" />
+                </div>
             </div>
             
-            <div>
-                <label htmlFor="major">Major</label>
-                <input type="text" name="sch-major" id="sch-major" />
-            </div>
-            
-            <div>
+            <div className="grad-holder">
                 <label htmlFor="grad-date">Graduation Date</label>
                 <input type="date" name="grad-date" id="grad-date" />
             </div>
 
-            <div>
+            <div className="btn-holder">
                 <button type="button" onClick={handleSave} className='btn'>{ saved ? 'Edit' : 'Save'}</button>
             </div>
         </div>
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,9 +1,11 @@
 import General from "./General";
+import Education from "./Education";
 
 export default function UserInput() {
     return (
         <>
             <General>General</General>
+            <Education>Education</Education>
         </>
     );
 }
