01db67ff4ca2b20d70fac717da52c575f0143fd0
Ayomikun Oyeniyi <ayomikunoyeniyi07@gmail.com>
2025-10-05 05:39:07 -0500

    Style Experience component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import '../styles/Experience.css';
 
 export default function Experience({children}) {
     const [saved, setSaved] = useState(false);
@@ -11,34 +12,45 @@ export default function Experience({children}) {
         <div className="experience">
             <h2>{children}</h2>
             <div className="cmp-info">
-                <div className='cmp-holder'>
-                    <label htmlFor="cmp-name">Company</label>
-                    <input type="text" name="cmp-name" id="cmp-name"/>
-                </div>
+                <div className="top-row">
+                    <div className='cmp-holder'>
+                        <label htmlFor="cmp-name">Company</label>
+                        <input type="text" name="cmp-name" id="cmp-name"/>
+                    </div>
             
-                <div className="role-holder">
-                    <label htmlFor="role">Role</label>
-                    <input type="text" name="role" id="role" />
+                    <div className="role-holder">
+                        <label htmlFor="role">Role</label>
+                        <input type="text" name="role" id="role" />
+                    </div>
                 </div>
+                
+                <div className="bot-row">
+                    <div className="start-holder">
+                        <label htmlFor="start-date">Start Date</label>
+                        <input type="date" name="start-date" id="start-date" />
+                    </div>
 
-                <div className="start-holder">
-                    <label htmlFor="start-date">Start Date</label>
-                    <input type="date" name="start-date" id="start-date" />
+                    <div className="end-holder">
+                        <label htmlFor="end-date">End Date</label>
+                        <input type="date" name="end-date" id="end-date" />
+                    </div>
                 </div>
-
-                <div className="end-holder">
-                    <label htmlFor="end-date">End Date</label>
-                    <input type="date" name="end-date" id="end-date" />
-                </div>
+                
             </div>
             
             <div className="rsp-holder">
                 <label htmlFor="responsibilties">Responsibilities</label>
-                <textarea type="date" name="responsibilites" id="responsibilties" />
+                <textarea name="responsibilities" id="responsibilities" />
             </div>
 
-            <div className="btn-holder">
-                <button type="button" onClick={handleSave} className='btn'>{ saved ? 'Edit' : 'Save'}</button>
+            <div className="exp-btn-holder">
+                <div className="add-btn-holder">
+                    <button type="button" className='add-btn'>Add</button>
+                </div>
+
+                <div className="save-btn-holder">
+                    <button type="button" onClick={handleSave} className='save-btn'>{ saved ? 'Edit' : 'Save'}</button>
+                </div>
             </div>
         </div>
     );
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,11 +1,13 @@
 import General from "./General";
 import Education from "./Education";
+import Experience from "./Experience";
 
 export default function UserInput() {
     return (
         <>
             <General>General</General>
             <Education>Education</Education>
+            <Experience>Experience</Experience>
         </>
     );
 }
