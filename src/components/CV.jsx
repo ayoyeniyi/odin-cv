77f6f9f07dee8195d3dc6b0c9d5ff3cc61959c07
Ayomikun Oyeniyi <ayomikunoyeniyi07@gmail.com>
2025-10-09 21:19:02 -0500

    Create basic structure for CV component

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -1,3 +1,12 @@
 export default function CV() {
-    
+    return (
+        <article>
+            <header></header>
+            <div className="info"></div>
+            <main>
+                <section></section>
+                <section></section>
+            </main>
+        </article>
+    )
 }
