2a36def5a7cfce4cc80993d036cb9a4ea135b67a
Ayomikun Oyeniyi <ayomikunoyeniyi07@gmail.com>
2025-10-09 21:45:52 -0500

    Define state object structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
 import UserInput from './components/UserInput';
 import CV from './components/CV';
 import './App.css';
 
 function App() {
-  
+  const [userData, setUserData] = useState({
+    general: {
+      name: "",
+      email: "",
+      phone: ""
+    },
+
+    education: {
+      school: "",
+      major: "",
+      gradDate: ""
+    },
+
+    experience: {
+      company: "",
+      role: "",
+      start: "",
+      end: "",
+      responsibilities: ""
+    }
+  });
+
   return (
     <>
     <div className='user-input'>
