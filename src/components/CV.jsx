fbc98be1caa7b55e1aa7bc3fdb813f8e367c979b
Ayomikun Oyeniyi <ayomikunoyeniyi07@gmail.com>
2025-10-14 20:56:55 -0500

    Add basic styling to cv header and info sections

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -1,12 +1,19 @@
+import '../styles/CV.css';
+
 export default function CV({
     general, 
     education,  
     experience, 
 }) {
     return (
-        <article>
-            <header></header>
-            <div className="info"></div>
+        <article className="cv-body">
+            <header className="cv-header">
+                <h1>{general.name}</h1>
+            </header>
+            <div className="cv-info">
+                <span className='cv-email'>{general.email}</span>
+                <span className='cv-number'>{general.phone}</span>
+            </div>
             <main>
                 <section></section>
                 <section></section>
